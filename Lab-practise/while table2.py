cfa2bf6bbe96ba9830c1c88adb3b7da9023c6845
9586884895 <hardik.isavani@gmail.com>
2025-06-23 11:03:10 +0530

    23-06-2023

---
+++ Lab-practise/while table2.py
@@ -0,0 +1,5 @@
+i=1
+a=int(input("enter a number:"))  #input from user
+while i<=10: #condition True
+    print(a,"*",i,"=",a*i)
+    i+=1
---
+++ Lab-practise/while2.py
@@ -0,0 +1,7 @@
+i=1 # initiate
+
+while i<=10: #condition True
+    print(i) #i=1
+    
+    i+=2
+
