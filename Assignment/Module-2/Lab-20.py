ee977560c6acb4dbab11ab4b6acd0ccb652de6bc
9586884895 <hardik.isavani@gmail.com>
2025-07-28 09:35:46 +0530

    Assignment

---
+++ Assignment/Module-2/Lab-20.py
@@ -0,0 +1,9 @@
+#Write a Python program to create a lambda function with one expression.
+
+square = lambda x: x ** 2
+
+n=int(input("enter the number:-"))
+result=square(n)
+
+print(f"The square of is {result}") 
+
---
+++ Assignment/Module-2/Lab-21.py
@@ -0,0 +1,4 @@
+# Write a Python program to create a lambda function with two expressions
+y=lambda a,b:a+b
+
+print(y(10,20))
